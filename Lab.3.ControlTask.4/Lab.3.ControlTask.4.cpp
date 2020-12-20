d0a9eae8859fb2beddd5734c59d313e6f20574ea
Олег Плугин <o.plugin@gmail.com>
2020-12-20 18:10:54 +0300

    Revert "Add Lab.3.ControlTask.3"
    This reverts commit 11ad7cb1754b68fd625340a62af249242f37e5cd.

---
+++ Lab.3.ControlTask.4/Lab.3.ControlTask.4.cpp
@@ -0,0 +1,27 @@
+﻿// Lab.3.ControlTask.4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int sum(int y, int x);
+
+int main()
+{
+	int a = 5, b = 10;
+
+	cout << sum(b, a) << endl;
+
+	return 0;
+}
+
+int sum(int y, int x)
+{
+	int s = 0;
+	if ((y - 1) == x)
+		s = y + x;
+	else
+		s = y + sum(y - 1, x);
+	return s;
+}
---
+++ Lab.4.ControlTask.1/Lab.4.ControlTask.1.cpp
@@ -0,0 +1,48 @@
+﻿// Lab.4.ControlTask.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double decremenant(double a, double b, double c, double& x1, double& x2);
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    cout << "Введите a, b , c" << endl;
+    double a, b, c, x1, x2;
+    cin >> a >> b >> c;
+
+    decremenant(a, b, c, x1, x2);
+}
+
+double decremenant(double a, double b, double c, double &x1, double &x2)
+{
+
+    double decremenant;
+    decremenant = b * b - 4 * a * c;
+    cout << "Декремент равен : " << decremenant << endl;
+
+    if (decremenant > 0)
+    {
+        x1 = (-b + sqrt(decremenant)) / (2 * a);
+        x2 = (-b - sqrt(decremenant)) / (2 * a);
+        cout << "x1 : " << x1 << " , x2 : " << x2 << endl;
+        return 1;
+    }
+    else if (decremenant == 0)
+    {
+        x1 = (-b) / (2 * a);
+        x2 = (-b) / (2 * a);
+        cout << "x1 : " << x1 << " , x2 : " << x2 << endl;
+        return 0;
+    }
+    else
+    {
+        cout << "корней нет" << endl;
+        return -1;
+    }
+}
