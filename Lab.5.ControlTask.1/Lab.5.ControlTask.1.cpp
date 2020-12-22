7750eb4308a55aeeab362e0d732260dcd4e37e0a
Олег Плугин <o.plugin@gmail.com>
2020-12-22 16:46:12 +0300

    Lab.5 Control Task upload

---
+++ Lab.5.ControlTask.1/Lab.5.ControlTask.1.cpp
@@ -0,0 +1,78 @@
+﻿// Lab.5.ControlTask.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/*
+    Лаб.5 Задание 1. Передача массива в функцию
+    Реализуйте созданный вами функционал в упражнениях 1 и 2 в виде
+    соответствующих функций, которые будут принимать массив и возвращать
+    требуемые результаты.
+    Функция должна принимать два параметра: целочисленное значение – размер
+    массива и сам массив.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void array_input(int arr[], const int SIZE);
+void arraySort(int a[], const int N);
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    const int SIZE = 10;
+    int mas[SIZE];
+    array_input(mas, SIZE);
+
+    arraySort(mas, SIZE);
+}
+
+void array_input(int arr[], const int SIZE)
+{
+    for (int i = 0; i < SIZE; i++)
+    {
+        cout << "mas[" << i << "]=";
+        cin >> arr[i];
+    }
+
+    int sum = 0;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        sum += arr[i];
+    }
+
+    int avg = sum / SIZE;
+
+    cout << "Сумма равна : " << sum << endl;
+    cout << "Среднее значение равно : " << avg << endl;
+}
+
+void arraySort(int a[], const int N)
+{
+    int min = 0; // для записи минимального значения 
+    int buf = 0; // для обмена значениями
+
+    for (int i = 0; i < N; i++)
+    {
+        min = i; // номер текущей ячейки, как ячейки с минимальным значением 
+     // в цикле найдем реальный номер ячейки с минимальным значением 
+        for (int j = i + 1; j < N; j++)
+            min = (a[j] < a[min]) ? j : min;
+        // перестановка этого элемента, поменяв его местами с текущим 
+        if (i != min)
+        {
+            buf = a[i];
+            a[i] = a[min];
+            a[min] = buf;
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << a[i] << '\t';
+    }
+        
+    cout << "\n" << endl;
+}
---
+++ Lab.5.ControlTask.2/Lab.5.ControlTask.2.cpp
@@ -0,0 +1,47 @@
+﻿// Lab.5.ControlTask.2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/*
+	Лаб.5 Задание 2. Возврат массива из функции
+	➢ В качестве результата работы функции может быть указатель на массив, создаваемый
+	в этой функции.
+	Дано: в функции main() объявлены два массива равного размера. В результате
+	вызова функции max_vect() возвращается массив, состоящий из больших
+	элементов, стоящих на соответствующих местах в исходных массивах
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int* max_vect(int kc, int a[], int b[]);
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int a[] = { 1,2,3,4,5,6,7,2 };
+	int b[] = { 7,6,5,4,3,2,1,3 };
+
+	int kc = sizeof(a) / sizeof(a[0]);	//Количество элементов массива 
+	int* c;								//Указатель на результирующий массив 
+	c = max_vect(kc, a, b);				//вызов функции для создания массива 
+
+	for (int i = 0; i < kc; i++)		//Вывод результата. 
+		cout << c[i] << " ";
+	cout << endl;
+	delete[] c;							//Возврат памяти. 
+}
+
+int* max_vect(int kc, int a[], int b[])
+{
+	for (int i = 0; i < kc; i++)
+	{
+		if (a[i] < b[i])
+		{
+			swap(a[i], b[i]);
+		}
+	}
+	return a;
+}
+
