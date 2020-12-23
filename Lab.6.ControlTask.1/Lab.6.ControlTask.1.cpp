eef4350650421d81e7b7e4bf133e9e649f93c2bb
Олег Плугин <o.plugin@gmail.com>
2020-12-23 17:09:11 +0300

    Lab.6 - Control Tasks 1, 2

---
+++ Lab.6.ControlTask.1/Lab.6.ControlTask.1.cpp
@@ -0,0 +1,53 @@
+﻿// Lab.6.ControlTask.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/*
+    Задание 1. Запись текста в файл
+    Требуется написать программу для записи небольшого стихотворения с
+    клавиатуры в текстовый файл.
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+    setlocale(LC_ALL, "ru");
+
+    string path = "myFile.txt";
+    ofstream fout;
+
+    fout.open(path, ofstream::app);
+
+    if (!fout.is_open())
+    {
+        cout << "Ошибка открытия файла!" << endl;
+    }
+    else
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            cout << "Введите строку стихотворения!" << endl;
+            string a;
+            cin >> a;
+            fout << a;
+            fout << "\n";
+        }
+    }
+
+    fout.close();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ Lab.6.ControlTask.2/Lab.6.ControlTask.2.cpp
@@ -0,0 +1,77 @@
+﻿// Lab.6.ControlTask.2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+bool from_min(const int a, const int b);
+bool from_max(const int a, const int b);
+void bubble_sort(int Arr[], const int N, bool (*compare)(int a, int b));
+
+void writeArray(int array[]);
+
+int main()
+{
+    system("chcp 1251");
+    //setlocale(LC_ALL, "rus");
+
+    const int SIZE = 10;
+    int my_choose = 0;
+
+    int array[SIZE] = { 9,8,7,6,1,2,3,5,4,9 };
+
+    writeArray(array);
+
+    switch (my_choose)
+    {
+    case 1: bubble_sort(array, SIZE, from_min);
+        break;
+    case 2: bubble_sort(array, SIZE, from_max);
+        break;
+    default:
+        cout << "\rНеизвестная операция ";
+    }
+
+    writeArray(array);
+
+}
+
+void writeArray(int array[])
+{
+    string path = "myFile.txt";
+    ofstream fout;
+    fout.open(path, ofstream::app);
+
+    if (!fout.is_open())
+    {
+        cout << "Ошибка открытия файла!" << endl;
+    }
+    else
+    {
+        cout << "Файл открыт!" << endl;
+        fout.write((char*)&array, sizeof(array));
+    }
+    fout.close();
+}
+
+bool from_min(const int a, const int b)
+{
+    return a > b;
+}
+bool from_max(const int a, const int b)
+{
+    return a < b;
+}
+
+void bubble_sort(int Arr[], const int N, bool (*compare)(int a, int b))
+{
+    for (int i = 1; i < N; i++)
+    {
+        for (int j = 0; j < N - 1; j++)
+        {
+            if ((*compare)(Arr[j], Arr[j + 1])) swap(Arr[j], Arr[j + 1]);
+        }
+    }
+}
