87a9a8d546ac12e92a5f74b835f37127f894373a
Олег Плугин <o.plugin@gmail.com>
2021-01-05 21:22:09 +0300

    Aggregation update

--- Lab.10.ControlTask.1/Lab.10.ControlTask.1.cpp
+++ Lab.10.ControlTask.1/Lab.10.ControlTask.1.cpp
@@ -1,38 +1,65 @@
 ﻿// Lab.10.ControlTask.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-/*
-* 
-Задание 1. Реализация класса Triangle 
-
-	В этом задании требуется создать класс Triangle, определяемый тремя точками – 
-	объектами соответсвующего класса Dot. 
-		Элементы класса Triangle: 
-		• Три точки – объекты класса Dot.
-		• Конструктор.
-		• Методы, позволяющие:
-		− вывести длины сторон треугольника;
-		− расчитать периметр треугольника;
-		− расчитать площадь треугольника
-		Класс Dot:
-		Определяется двумя координатами и функцией – расстоянием между точками.
-*/
-
 #include <iostream>
 #include <cmath>
-#include "Triangle.h"
-#include "Dot.h"
 
 using namespace std;
 
+class Dot
+{
+
+private:
+	double x;
+	double y;
+
+public:
+	Dot() : x(0), y(0) { }
+
+	Dot(double dx, double dy) : x(dx), y(dy) { }
+
+	double distanceTo(Dot point)
+	{
+		return sqrt(pow(point.x - x, 2) + pow(point.y - y, 2));
+	}
+};
+
+class Triangle
+{
+private:
+	Dot& ta;
+	Dot& tb;
+	Dot& tc;
+
+public:
+	Triangle(Dot& d1, Dot& d2, Dot& d3) : ta(d1), tb(d2), tc(d3) {}
+
+	void TriangleShowSide()
+	{
+		cout << "Длинна стороны А равна : " << ta.distanceTo(tb) << endl;
+		cout << "Длинна стороны B равна : " << tb.distanceTo(tc) << endl;
+		cout << "Длинна стороны C равна : " << tc.distanceTo(ta) << endl;
+	}
+	void PerimetrTriangle()
+	{
+		cout << "Периметр треугольника равен : "
+			<< ta.distanceTo(tb) + tb.distanceTo(tc) + tc.distanceTo(ta) << endl;
+	}
+	void SqareTriangle()
+	{
+		double p = (ta.distanceTo(tb) + tb.distanceTo(tc) + tc.distanceTo(ta)) / 2;
+		double s = sqrt(p * (p - ta.distanceTo(tb) * (p - tb.distanceTo(tc)) * (p - tc.distanceTo(ta))));
+		cout << "Площадь треугольника равна : " << s;
+	}
+};
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
 
-	Dot a(2, 3);
-	Dot b(3, 5);
-	Dot c(1, 6);
+	Dot a(5.0, 4.0);
+	Dot b(1.0, 3.0);
+	Dot c(6.0, 8.0);
 
 	Triangle tr(a, b, c);
 	cout << "Длинны сторон" << endl;
